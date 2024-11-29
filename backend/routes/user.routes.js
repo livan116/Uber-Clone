8df4900fe4b73fbff607e3145f3f8337b8adfe3c
livan116 <livankumar112@gmail.com>
2024-11-29 18:08:22 +0530

    user registration and login

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const router = express.Router();
+const { body } = require("express-validator");
+const userController = require('../controller/user.controller')
+
+router.post("/register", [
+  body("email").isEmail().withMessage("Invalid Email"),
+  body("fullname.firstname")
+    .isLength({ min: 3 })
+    .withMessage("First name must be atleast 3 characters long"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be atleast 6 characters long"),
+
+    userController.registerUser
+]);
+
+router.post('/login',[
+  body('email').isEmail().withMessage('Invalid Email'),
+  body('password').isLength({min:6}).withMessage('Password must be atleast 6 characters long')
+],
+userController.loginUser)
+
+router.get('/profile',userController.getUserProfile)
+
+module.exports = router;
